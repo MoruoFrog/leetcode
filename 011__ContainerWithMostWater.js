5c5184e3fdc6193c112fdc6a231ee2f396428985
moruo44 <734733711@qq.com>
2018-11-05 16:48:39 +0800

    :punch: 011 Container Wit hMost Water

---
+++ 011__ContainerWithMostWater.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    const len = height.length
+    let i = 0,
+        j = len - 1,
+        result = 0
+    
+    while(i !== j) {
+        const top = height[i]
+        const tail = height[j]
+        result = Math.max(result, Math.min(top, tail) * (j - i))
+        
+        const nextTop = height[i+1]
+        const nextTail = height[j - 1]
+        // if move the taller side, the result must decrease，so no need to do that
+        if(tail >= top) {
+            i++
+        }else {
+            j--
+        }
+    }
+	return result
+};
