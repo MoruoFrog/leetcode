c0d346634bd8f76a3d6e88409c748664f28735a0
moruo44 <734733711@qq.com>
2018-12-11 17:22:59 +0800

    :art: code style fix

--- 094__BinaryTreeInorderTraversal.js
+++ 094__BinaryTreeInorderTraversal.js
@@ -14,49 +14,49 @@
 var isLeaf = (node) => !(node.left || node.right)
 
 var inorderTraversal = function(root) {
-  const stack = []
-  const result = []
+    const stack = []
+    const result = []
 
-  let current = root
-  let isTrackBack = false
+    let current = root
+    let isTrackBack = false
 
-  while(current) {
-    if (isLeaf(current)) {
-      result.push(current.val)
-      current = stack.pop()
-      isTrackBack = true
-      continue
+    while (current) {
+        if (isLeaf(current)) {
+            result.push(current.val)
+            current = stack.pop()
+            isTrackBack = true
+            continue
+        }
+
+        if (isTrackBack) {
+            result.push(current.val)
+
+            if (current.right) {
+                current = current.right
+                isTrackBack = false
+            } else {
+                current = stack.pop()
+                isTrackBack = true
+            }
+        } else {
+            stack.push(current)
+            if (current.left) {
+                current = current.left
+                isTrackBack = false
+            } else {
+                current = stack.pop()
+                isTrackBack = true
+            }
+        }
     }
 
-    if (isTrackBack) {
-      result.push(current.val)
-
-      if (current.right) {
-        current = current.right
-        isTrackBack = false
-      } else {
-        current = stack.pop()
-        isTrackBack = true
-      }
-    }else {
-      stack.push(current)
-      if (current.left) {
-        current = current.left
-        isTrackBack = false
-      } else {
-        current = stack.pop()
-        isTrackBack = true
-      }
-    }
-  }
-
-  return result
+    return result
 }
 
 // recursion
-var inorderTraversal = function(root) {
+var inorderTraversal = function (root) {
     if (!root) return []
-    
+
     const leftInorder = inorderTraversal(root.left)
     const rightInorder = inorderTraversal(root.right)
     return [...leftInorder, root.val, ...rightInorder]
